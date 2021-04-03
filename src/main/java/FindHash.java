cc5d8503f05a2590e5e689db62fb6b5f66b723bd
SgWarior <techies008@gmail.com>
2021-04-03 22:14:13 +0300

    Hi Pavel, is version 0.0009

--- src/main/java/FindHash.java
+++ src/main/java/FindHash.java
@@ -15,9 +15,12 @@ public class FindHash extends SimpleFileVisitor {
             File current = new File(file.toString());
             try(BufferedReader reader = new BufferedReader(new FileReader(current))){
                 while (reader.ready()){
-                    String tmp = reader.readLine();
-                    if (tmp.contains("BC1YLgoGKCGAWNUfx9JSRoYUk1CWBsP3cB9FfAWxe8AQbnxhVDP8jSD")) {
-                        output.write(current.toString()+tmp);
+                    String fLine = reader.readLine();
+                    String sLine = reader.readLine();
+                    if (fLine.contains("\"TxnType\": \"CREATOR_COIN\"") &&
+                            sLine.contains("BC1YLgoGKCGAWNUfx9JSRoYUk1CWBsP3cB9FfAWxe8AQbnxhVDP8jSD")
+                    ) {
+                        output.write(current.toString()+sLine);
                         output.newLine();
                     }
                 }
